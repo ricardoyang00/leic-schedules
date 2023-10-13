08db445ea653d74be603ec9a9a25ebc7fcc19026
Huang <brunohuang2012gmail.com>
2023-10-13 08:14:14 +0100

    Probably data isnt stored correctly

--- Code/Functions.cpp
+++ Code/Functions.cpp
@@ -8,12 +8,16 @@ Functions::Functions() {
 }
 
 void Functions::ConsultarHorarioAluno(int studentCode) {
-    for (const auto& student : students) {
-        if (student.StudentCode == studentCode) {
-            cout << "Número do Aluno(a): " << student.StudentCode << endl;
+    for (const Student& student : students) {
+        if (studentCode == student.StudentCode) {
+            cout << "Número do Aluno(a): " << studentCode << endl;
             cout << "Nome do Aluno(a): " << student.StudentName << endl;
-
+            cout << "Códigos da UC e códigos da respetiva Turma: ";
+            for (const auto& pair : student.UcToClasses) {
+                cout << pair.first << ", " << pair.second << endl;
+            }
             return;
         }
     }
+    cout << "Student with code " << studentCode << " not found." << endl;
 }
--- Code/tests.cpp
+++ Code/tests.cpp
@@ -3,7 +3,7 @@
 int main() {
     Functions functions;
 
-    int number = 202040617;
+    int number = 202025232;
     functions.ConsultarHorarioAluno(number);
 
     return 0;
