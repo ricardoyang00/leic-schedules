a04a00c4e3798a28bfba937020d2d109db2509db
ricardoyang00 <ricardo.yang00@gmail.com>
2023-10-27 15:09:45 +0100

    Bruno functions added to consult file

--- Code/Consult.cpp
+++ Code/Consult.cpp
@@ -340,3 +340,60 @@ void Consult::consultOccupationOfYear_descendingOrder(int year) {
         cout << entry.second << ": " << entry.first << endl;
     }
 }
+
+void Consult::ListStudentsWithSameName() {
+    cout << "Enter the student you want to search for: ";
+    string searchName;
+    cin >> searchName;
+    //searchName = ToLower(searchName);
+
+    vector<Student> matchingStudents;
+
+    // Search for students with the exact same name using the binary search tree
+    globalData.Students.searchAllByName(searchName, matchingStudents);
+
+    // Student not found
+    if (matchingStudents.empty()) {
+        cout << "Student with name: " << searchName << " not found." << endl;
+        return;
+    }
+
+    // List all students with "searchName" and allow user to select one
+    //system("cls"); // Windows
+    system("clear"); // Unix-like system
+    cout << "\033[1mSearch criteria:\033[0m (Name: " << searchName << ")" << endl;
+    cout << "\n";
+    cout << "\033[1mResults of the search:\033[0m " << endl;
+    for (int i = 0; i < matchingStudents.size(); i++) {
+        cout << i + 1 << ". " << "Student Code: " << matchingStudents[i].StudentCode
+             << "     Name: " << matchingStudents[i].StudentName << endl;
+    }
+    cout << "\n";
+
+    int choice;
+    while (true) {
+        cout << "Enter the number of the student you want to view: ";
+        cin >> choice;
+
+        // Check if user's choice is valid
+        if (choice >= 1 && choice <= matchingStudents.size()) {
+            system("clear");
+            break;
+        }
+
+        cout << "Invalid choice. PLease enter a valid number. " << endl << flush;
+        cout << "\n";
+    }
+
+    // Output chosen student's information
+    const Student chosenStudent = matchingStudents[choice - 1];
+    cout << "\n";
+    cout << "\033[1mSelected student information:\033[0m " << endl;
+    cout << "Student Code: " << chosenStudent.StudentCode << endl;
+    cout << "Student Name: " << chosenStudent.StudentName << endl;
+    cout << "UCs and Classes: " << endl;
+    for (const Class& ucToClass : chosenStudent.UcToClasses) {
+        cout << "UcCode: " << ucToClass.UcCode  << ", ClassCode: " << ucToClass.ClassCode << endl;
+    }
+    cout << "\n";
+}
